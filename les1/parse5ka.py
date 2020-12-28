5af01dac32778e615a49b2589f4c8fe1b5c947c0
maximka <maxim652002@gmail.com>
2020-12-28 14:24:07 +0200

    les1 init V2.0

--- les1/parse5ka.py
+++ les1/parse5ka.py
@@ -1,66 +1,85 @@
+import json
 import requests
 from pathlib import Path
 import time
-import json
-
-headers = {
-    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36'
-}
-
-params = {
-    'records_per_page': 50,
-    'page': 1,
-}
-url = 'https://5ka.ru/api/v2/special_offers/'
-response = requests.get(url, headers=headers)
-
-with open('5ka.html', 'w', encoding='UTF-8') as file:
-    file.write(response.text)
-
-
-class StatusCodeError(Exception):
-    def __init__(self, txt):
-        self.txt = txt
 
 
 class Parser5ka:
-    headers = {
-        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36'
+    _params = {
+        'records_per_page': 50,
+    }
+    _headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_1_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36',
     }
 
     def __init__(self, start_url):
         self.start_url = start_url
 
-    def _get_response(self, url, **kwargs):
+    @staticmethod
+    def _get(*args, **kwargs):
         while True:
             try:
-                response = requests.get(url, **kwargs)
+                response = requests.get(*args, **kwargs)
                 if response.status_code != 200:
-                    raise StatusCodeError(f'status {response.status_code}')
+                    raise Exception
+                time.sleep(0.2)
                 return response
-            except (requests.exceptions.ConnectTimeout,
-                    StatusCodeError):
-                time.sleep(0.1)
+            except Exception:
+                time.sleep(0.3)
 
     def run(self):
         for products in self.parse(self.start_url):
             for product in products:
-                file_path = Path(__file__).parent.joinpath(
-                    f'{product["id"]}.json')
-                self.save_file(file_path, product)
+                self.save_to_json_file(product, product['id'])
 
     def parse(self, url):
+        if not url:
+            url = self.start_url
+        params = self._params
         while url:
-            response = self._get_response(url, headers=self.headers)
-            data = response.json()
-            url = data['next']
-            yield data.get('results', [])
+            response = self._get(url, params=params, headers=self._headers)
+            if params:
+                params = {}
+            data: dict = response.json()
+            url = data.get('next')
 
-    def save_file(self, file_path: Path, data: dict):
-        with open(file_path, 'w', encoding='UTF-8') as file:
-            json.dump(data, file)
+            yield data.get('results')
+
+    @staticmethod
+    def save_to_json_file(data: dict, file_name):
+        with open(f'products/{file_name}.json', 'w', encoding='UTF-8') as file:
+            json.dump(data, file, ensure_ascii=False)
+
+
+class ParserCatalog(Parser5ka):
+
+    def __init__(self, start_url, category_url):
+        self.category_url = category_url
+        super().__init__(start_url)
+
+    def get_categories(self, url):
+        response = requests.get(url, headers=self._headers)
+        return response.json()
+
+    def run(self):
+        for category in self.get_categories(self.category_url):
+            data = {
+                "name": category['parent_group_name'],
+                'code': category['parent_group_code'],
+                "products": [],
+            }
+
+            self._params['categories'] = category['parent_group_code']
+
+            for products in self.parse(self.start_url):
+                data["products"].extend(products)
+            self.save_to_json_file(
+                data,
+                category['parent_group_code']
+            )
 
 
 if __name__ == '__main__':
-    parser = Parser5ka('https://5ka.ru/api/v2/special_offers/')
+    parser = ParserCatalog(
+        'https://5ka.ru/api/v2/special_offers/', 'https://5ka.ru/api/v2/categories/')
     parser.run()
